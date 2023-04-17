40d2d6a72211981ddd43342b673129e0080dfd26
Drumzan <120139387+Drumzan@users.noreply.github.com>
2023-04-17 13:13:54 +0200

    Update Constants.java

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -70,7 +70,7 @@ public class Constants {
     public static class PlayerConstants{
         public static final int IDLE = 0;
         public static final int RUNNING = 1;
-        public static final int JUMP = 3;
+        public static final int JUMP = 2;
 
         public static int GetSpriteAmount(int player_action){
             switch (player_action){
