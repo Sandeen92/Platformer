e1ddec47908729b45b86713fdd8973ff49474f5a
Drumzan <120139387+Drumzan@users.noreply.github.com>
2023-05-02 11:43:04 +0200

    changes variables

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -104,9 +104,9 @@ public class Constants {
 
     public static class GameConstants{
         public final static float SCALE = 2f;
-        public final static int TILES_DEFAULT_SIZE = 32;
-        public final static int TILES_IN_WIDTH = 26;
-        public final static int TILES_IN_HEIGHT = 14;
+        public final static int TILES_DEFAULT_SIZE = 16;
+        public final static int TILES_IN_WIDTH = 52;
+        public final static int TILES_IN_HEIGHT = 28;
         public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
         public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
         public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
